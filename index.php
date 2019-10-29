1042123340b6e9d6283c41feef1f8ca3e87d3796
mariuskkt <marius.kukta@gmail.com>
2019-10-29 09:30:50 +0100

    pirmas commit

--- index.php
+++ index.php
@@ -9,8 +9,7 @@
 <html>
     <head>
         <title>Aš valdau HTML!</title>
-        <meta charset="windows-1252">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
     </head>
     <body>
         <h1> Marius- html asas!</h1>
