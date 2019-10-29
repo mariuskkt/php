b6b7a83dbe5106f5a1cb493002a4b72ea3c05f72
mariuskkt <marius.kukta@gmail.com>
2019-10-29 09:26:17 +0100

    pirmas commit

--- index.php
+++ index.php
@@ -6,4 +6,15 @@
  * and open the template in the editor.
  */
 
-print 'pakeitimas';
+<html>
+    <head>
+        <title>Aš valdau HTML!</title>
+        <meta charset="windows-1252">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <h1> Marius- html asas!</h1>
+        <p> Moku kurti statin? turin?</p>
+            
+    </body>
+</html>
