bb5af15a6e229adde40b8cfe95c3d0950100f080
mariuskkt <marius.kukta@gmail.com>
2019-10-29 10:16:21 +0100

    pirmas commit

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 
 <html>
     <head>
-        <title>Aš valdau HTML <?PHP print 'ir PHP'?></title>
+        <title>Aš valdau HTML <?PHP print 'ir PHP'; ?></title>
         </head>
     <body>
-        <h1>Marius - html <?php print 'ir php asas'?>!</h1>
+        <h1>Marius - html <?php print 'ir php'; ?> asas!</h1>
         <p> Kuriu dinamin? turin?</p>
-        <span>Atidaryta: <?php print date ('Y-m-d H:i:s');?></span> 
+        <span>Atidaryta: <?php print date ('Y-m-d H:i:s'); ?></span> 
     </body>
 </html>
