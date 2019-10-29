cf5d42eca72778ca379f9208a675315110ff479e
mariuskkt <marius.kukta@gmail.com>
2019-10-29 11:11:04 +0100

    pirmas commit

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
 
 <html>
     <head>
-        <title> <?php print 'Aš,' . date('l') . 'ir PHP'; ?></title>
+        <title> <?php print 'Aš,' . date('l') . ' ir PHP'; ?></title>
+        <meta charset="UTF-8">
+
         </head>
     <body>
-        <h1> <b>Marius</b> - html <?php print 'ir php'; ?> asas! <br>
-        jau nuo <?php print date('Y'). 'met?'; ?>
+        <h1> <b>Marius</b> - html <?php print ' ir php'; ?> asas! <br>
+        jau nuo <?php print date(' Y'). ' met?'; ?>
         </h1>
-        <p> Viskas prasid?jo <?php print date('F'); ?> <br>
-               <?php print 'menesio,' . date('j') . 'dien?!'; ?> </p>
+        <p> Viskas prasid?jo <?php print date(' F'); ?> <br>
+               <?php print ' menesio,' . date(' j') . ' dien?!'; ?> </p>
         
     </body>
 </html>
