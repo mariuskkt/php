5e49c3d717151f6ed01841e84f81f7d4cf356425
mariuskkt <marius.kukta@gmail.com>
2019-10-30 09:17:26 +0100

    pirmas commit

--- index.php
+++ index.php
@@ -2,12 +2,18 @@
 
 <html>
     <head>
-        <title>Php lydes ir <?php print date('Y.m.d', strtotime('+' . rand(1, 5) . 'years')); ?></title>  
+        <title></title>
+        <style>
+            body{background-color: rgb(<?php print rand(0, 255); ?>, <?php print rand(0, 255); ?>, <?php print rand(0, 255); ?>);}
+            h1{font-size: <?php print rand(1, 70);  ?> px;}
+            p {color: rgb(<?php print rand(0, 255); ?>, <?php print rand(0, 255); ?>, <?php print rand(0, 255); ?>);}
+        </style>
     </head>
     <body>
-        <h1><strong>Marius</strong> - Galbut turesiu <?php print (rand(1, 5) . ' vaiku !'); ?> </h1>
-        <p> D. Trump'as nebebus prezidentu:<?php print date(' Y.m.d' , strtotime('+' . rand(2, 10) . 'years')); ?>
-        </p>
+        <main>
+            <h1>As keiciu savo dydi!</h1>
+            <p> As keiciu savo spalva!</p>
+        </main>
     </body>
+</head>
 </html>
-
