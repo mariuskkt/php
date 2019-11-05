120c3ead4ad63d01370b8c8b4755417247d9bbe3
mariuskkt <marius.kukta@gmail.com>
2019-11-05 14:53:49 +0100

    pirams commit

--- index.php
+++ index.php
@@ -1,7 +1,22 @@
 <?php
+$months = 12;
+$wallet = 1000;
+$month_income = 700;
 
-$x = rand(1, 10);
-
-for ($x=0; $x<10; $x++){
-var_dump("Tai yra $x-tasis ciklas");
+for ($x = 1; $x <= $months; $x++) {
+    $month_expenses = rand(300, 699);
+    $wallet += $month_income - $month_expenses;
 }
+
+$h1 = 'Wallet forecast';
+$h2 = "Po $months m., prognozuojamas likutis: $wallet";
+?>
+<html>
+    <head>
+        <title>Wallet</title>
+    </head>
+    <body>
+        <h1><?php print $h1; ?></h1>
+        <h2><?php print $h2; ?></h2>
+    </body>
+</html>
