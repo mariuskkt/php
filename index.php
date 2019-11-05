ed3f5c531a097664fcf073c173f820ce24abe7af
mariuskkt <marius.kukta@gmail.com>
2019-11-05 13:40:47 +0100

    pirmas commit

--- index.php
+++ index.php
@@ -1,26 +1,7 @@
 <?php
 
-$string = 'vienas';
-$int = 123;
-$bool = true;
-$null = null;
+$x = rand(1, 10);
 
-$li_1 = "String tipas: $string";
-$li_2 = "Integer tipas: $int";
-$li_3 = "Boolean tipas: $bool";
-$li_4 = "Null_tipas: $null";
-
-?>
-<!DOCTYPE html>
-<html>
-    <head>
-    </head>
-    <body>
-        <ul>
-            <li><?php print $li_1; ?></li>
-            <li><?php print $li_2; ?></li>
-            <li><?php print $li_3; ?></li>
-            <li><?php print $li_4; ?></li>
-        </ul>
-    </body>
-</html>
+for ($x=0; $x<10; $x++){
+var_dump("Tai yra $x-tasis ciklas");
+}
