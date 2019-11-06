0872503b2fcf04b16bcb5f4c0d401b2a350f35cd
mariuskkt <marius.kukta@gmail.com>
2019-11-06 18:00:16 +0100

    pirmas commit

--- index.php
+++ index.php
@@ -5,26 +5,26 @@ $depreciation = 2;
 $car_price = 30000;
 
 
-for ($months = 0; $months < 24; $months++){
-    
-$car_price_new -= round ($car_price_new * $depreciation /100);
+for ($months = 0; $months < 24; $months++) {
+
+    $car_price -= round($car_price * $depreciation / 100);
 }
 
-$procentai = ($car_price - $car_price_new) *100 / $car_price;
+$procentai = ($car_price_new - $car_price) * 100 / $car_price_new;
 
 $h1 = 'Kiek nuvertes masina?';
-$h2 = "Naujos masinos kaina: $car_price";
-$h3 = "Po $months men, masinos verte bus: $car_price_new eur";
+$h2 = "Naujos masinos kaina: $car_price_new eur";
+$h3 = "Po $months men, masinos verte bus: $car_price eur";
 $h4 = "Masina nuvertes $procentai %";
 ?>
 <html>
-   <head>
-       <title>Car</title>
-   </head>
-   <body>
-       <h1><?php print $h1; ?></h1>
-       <h2><?php print $h2; ?></h2>
-       <h3><?php print $h3; ?></h3>
-       <h4><?php print $h4; ?></h4>
-   </body>
+    <head>
+        <title>Car</title>
+    </head>
+    <body>
+        <h1><?php print $h1; ?></h1>
+        <h2><?php print $h2; ?></h2>
+        <h3><?php print $h3; ?></h3>
+        <h4><?php print $h4; ?></h4>
+    </body>
 </html>
