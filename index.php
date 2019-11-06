b714ab21ed93aff64eb7e09c86f2efef78388e40
mariuskkt <marius.kukta@gmail.com>
2019-11-06 16:47:16 +0100

    pirmas commit

--- index.php
+++ index.php
@@ -1,22 +1,30 @@
 <?php
-$months = 12;
-$wallet = 1000;
-$month_income = 700;
+$months = 24;
+$car_price_new = 30000;
+$depreciation = 2;
+$car_price = 30000;
 
-for ($x = 1; $x <= $months; $x++) {
-    $month_expenses = rand(300, 699);
-    $wallet += $month_income - $month_expenses;
+
+for ($months = 0; $months < 24; $months++){
+    
+$car_price_new -= round ($car_price_new * $depreciation /100);
 }
 
-$h1 = 'Wallet forecast';
-$h2 = "Po $months m., prognozuojamas likutis: $wallet";
+$procentai = ($car_price - $car_price_new) *100 / $car_price;
+
+$h1 = 'Kiek nuvertes masina?';
+$h2 = "Naujos masinos kaina: $car_price";
+$h3 = "Po $months men, masinos verte bus: $car_price_new eur";
+$h4 = "Masina nuvertes $procentai %";
 ?>
 <html>
-    <head>
-        <title>Wallet</title>
-    </head>
-    <body>
-        <h1><?php print $h1; ?></h1>
-        <h2><?php print $h2; ?></h2>
-    </body>
+   <head>
+       <title>Car</title>
+   </head>
+   <body>
+       <h1><?php print $h1; ?></h1>
+       <h2><?php print $h2; ?></h2>
+       <h3><?php print $h3; ?></h3>
+       <h4><?php print $h4; ?></h4>
+   </body>
 </html>
