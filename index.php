3dd7cc2401b03d4099a9eb3247b422eb266b68ee
mariuskkt <57089542+mariuskkt@users.noreply.github.com>
2020-02-25 08:46:07 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
     $piss_temp = 36.4;
 
     $piss_soup_temp = ($soup_ml * $soup_temp + $piss_ml * $piss_temp)/ ($soup_ml + $piss_ml);
+    $h1 = "Sriubos prognozė";
+    $p1 = "Pradžioje puode buvo" .  <?php print $soup_ml?> . "ml." . <?php print $soup_temp ?> . "C sriubos.";
+    $p2 = "Į puodą primyžus" . <?php print $piss_ml ?> . "ml.," . "sriubos temperatūra patapo" . <?php print $piss_soup_temp ?> . "C."; 
 ?>
 
 <html>
@@ -12,8 +15,8 @@
     <title>Sysaline</title>
 </head>
 <body>
-    <h1>Sriubos prognozė</h1>
-    <p>Pradžioje puode buvo <?php print $soup_ml?> ml. <?php print $soup_temp ?> C sriubos. </p>
-    <p>Į puodą primyžus <?php print $piss_ml ?> ml., sriubos temperatūra patapo <?php print $piss_soup_temp ?> C. </p>
+    <h1><?php print $h1; ?></h1>
+    <p> <?php print $p1; ?></p>
+    <p><?php print $p2; ?></p>
 </body>
 </html>
